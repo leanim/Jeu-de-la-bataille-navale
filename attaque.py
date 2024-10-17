879a86ab45ce2bd7f9757a3609cf450fe110c34a
Lea <lea.chen@edu.esiee-it.fr>
2024-10-17 21:57:19 +0200

    Ajout d'une fonction qui permet au joueur de disposer de tir unique et valide

--- attaque.py
+++ attaque.py
@@ -1,16 +1,24 @@
 #Attaquer la grille de l'ennemi
 def attaquer(grilleennemi):
-    colonne=input("Entrez la colonne à attaquer (A-J)")
-    ligne=int(input("Entrez la ligne à attaquer (1-10)"))
-    colonne= ord(colonne) - ord("A") 
+    while True:
+        colonne=input("Entrez la colonne à attaquer (A-J)")
+        ligne=int(input("Entrez la ligne à attaquer (1-10)"))
+        colonne= ord(colonne) - ord("A") 
 
-    if 0<=ligne<10 and 0 <= colonne<10:
-        if grilleennemi[ligne][colonne]=="O":
-            print("Touché")
+        if 0<=ligne<10 and 0 <= colonne<10:
+            if grilleennemi[ligne][colonne]=="O":
+             print("Touché")
             grilleennemi[ligne][colonne]="X"
+        elif grilleennemi[ligne][colonne]=="X":
+            print("Cette case a déjà été attaqué, veuilez viser une autre case")
+        elif grilleennemi[ligne][colonne]=="T":
+            print("Cette case a déjà été attaqué, veuilez viser une autre case")
+
         else:
             print("Coulé")
             grilleennemi[colonne][ligne]="T"
+            break
+            
             
 #Attaque du joueur
 for joueur in range(2):
